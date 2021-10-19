183314abd382042a12f543ba1ef9f915eec574b6
meme2367 <meme91322367@gmail.com>
2021-10-19 17:24:32 +0900

    refactor : ReadMenuCommand 코드 수정

--- src/main/java/com/enjoydelivery/dto/store/response/ReadMenuCommand.java
+++ src/main/java/com/enjoydelivery/dto/store/response/ReadMenuCommand.java
@@ -1,6 +1,7 @@
 package com.enjoydelivery.dto.store.response;
 
 import com.enjoydelivery.entity.Menu;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ReadMenuCommand {
 
+  @NotNull
   private Long id;
+  @NotNull
   private String name;
+  @NotNull
   private int price;
 
   public ReadMenuCommand(Menu menu) {
     this.id = menu.getId();
     this.name = menu.getName();
     this.price = menu.getPrice();
+  }
+
+  public Menu toEntity() {
+    return Menu.builder()
+        .id(id)
+        .name(name)
+        .price(price)
+        .build();
   }
 }
