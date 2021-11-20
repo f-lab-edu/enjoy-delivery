160c37f781951ec5cd7ec1cb7f8fb944d7fb0f33
meme2367 <meme91322367@gmail.com>
2021-11-20 14:52:42 +0900

    feat : Store Service에 Exception message 작성

--- src/main/java/com/enjoydelivery/service/StoreService.java
+++ src/main/java/com/enjoydelivery/service/StoreService.java
@@ -13,35 +13,41 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class StoreService {
 
+  public static final String EXCEPTION_NOT_FOUND_STORE = "조회되는 가게 데이터가 없습니다.";
+  public static final String EXCEPTION_DUPLICATE_NAME = "이미 등록된 가게 이름입니다.";
+  public static final String EXCEPTION_NOT_FOUND_MENUS = "조회되는 메뉴 데이터가 없습니다.";
+  public static final String EXCEPTION_NOT_FOUND_CATEGORY = "조회되는 카테고리 데이터가 없습니다.";
   private final StoreRepository storeRepository;
   private final CategoryService categoryService;
+
+
 
   public List<Store> readAllByCategory(Long categoryId) {
     Category findCategory = categoryService.readOneById(categoryId);
     List<Store> stores = storeRepository.findStoreFetchJoinByCategory(findCategory);
     if (stores == null || stores.isEmpty()) {
-      throw new RuntimeException();
+      throw new RuntimeException(EXCEPTION_NOT_FOUND_STORE);
     }
     return stores;
   }
 
   public Store readOneFetchJoinById(Long storeId) {
     Store findStore = storeRepository.findDistinctStoreFetchJoinById(storeId)
-        .orElseThrow(RuntimeException::new);
+        .orElseThrow(() -> new RuntimeException(EXCEPTION_NOT_FOUND_STORE));
 
     if (findStore.getCategory() == null) {
-      throw new RuntimeException();
+      throw new RuntimeException(EXCEPTION_NOT_FOUND_CATEGORY);
     }
 
     if (findStore.getMenus() == null) {
-      throw new RuntimeException();
+      throw new RuntimeException(EXCEPTION_NOT_FOUND_MENUS);
     }
     return findStore;
   }
 
   public Store readOneById(Long storeId) {
     return storeRepository.findById(storeId)
-        .orElseThrow(RuntimeException::new);
+        .orElseThrow(() -> new RuntimeException(EXCEPTION_NOT_FOUND_STORE));
   }
 
   public void create(StoreRequestDTO storeRequestDTO) {
@@ -50,7 +56,7 @@ public class StoreService {
       storeRepository.save(store);
       return;
     }
-    throw new RuntimeException();
+    throw new RuntimeException(EXCEPTION_DUPLICATE_NAME);
   }
 
   @Transactional
@@ -61,7 +67,7 @@ public class StoreService {
 
   public Store readOneByOwnerId(Long ownerId) {
     return storeRepository.findStoreByOwner_Id(ownerId)
-        .orElseThrow(RuntimeException::new);
+        .orElseThrow(() -> new RuntimeException(EXCEPTION_NOT_FOUND_STORE));
 
   }
 }
