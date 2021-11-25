03107ff54f6b34d909964bb29b22c57232435f2a
meme2367 <meme91322367@gmail.com>
2021-11-25 10:17:04 +0900

    fix : 테스트 코드 버그 수정

--- src/test/java/com/enjoydelivery/service/CartServiceTest.java
+++ src/test/java/com/enjoydelivery/service/CartServiceTest.java
@@ -36,7 +36,7 @@ public class CartServiceTest {
   @InjectMocks
   CartService cartService;
 
-  private CreateOrderItemRequestDTO makeCreateOrderItemRequestDTO() {
+  public CreateOrderItemRequestDTO makeCreateOrderItemRequestDTO() {
     return new CreateOrderItemRequestDTO(
         1L,
         1L,
@@ -44,18 +44,18 @@ public class CartServiceTest {
         3);
   }
 
-  private ReadMenuResponseDTO makeReadMenuResponseDTO() {
+  public ReadMenuResponseDTO makeReadMenuResponseDTO() {
     return new ReadMenuResponseDTO(
         1L,
         "엽기떡볶이",
         17000);
   }
 
-  private OrderItem makeOrderItem() {
+  public OrderItem makeOrderItem() {
     return makeOrderItem(makeCreateOrderItemRequestDTO());
   }
 
-  private OrderItem makeOrderItem(CreateOrderItemRequestDTO createOrderItemRequestDTO) {
+  public OrderItem makeOrderItem(CreateOrderItemRequestDTO createOrderItemRequestDTO) {
     return createOrderItemRequestDTO.toEntity();
   }
   public StoreRequestDTO makeStoreRequestDTO() {
@@ -86,7 +86,7 @@ public class CartServiceTest {
 
   @Test
   @DisplayName("장바구니가 비어있을 때에는 메뉴를 추가할 수 있다.")
-  private void addOrderItemSuccess() {
+  void addOrderItemSuccess() {
 
     // Arrange
     CreateOrderItemRequestDTO createOrderItemRequestDTO
@@ -96,8 +96,9 @@ public class CartServiceTest {
     Long userId = createOrderItemRequestDTO.getUserId();
     Long storeId = createOrderItemRequestDTO.getStoreId();
 
-    doReturn(0)
-        .when(cartDAO.findStoreIdByUserId(userId));
+    doReturn(0L)
+        .when(cartDAO)
+        .findStoreIdByUserId(userId);
 
     // Act
     cartService.addOrderItem(createOrderItemRequestDTO);
@@ -105,18 +106,17 @@ public class CartServiceTest {
     // Assert
     verify(cartDAO, times(1))
         .addOrderItem(
-            argThat(od -> od.getId() == orderItem.getId()
-                && od.getCount() == orderItem.getCount()
-                && od.getMenu().equals(orderItem.getMenu())),
-            argThat(u -> u == userId),
-            argThat(s -> s == storeId));
+            argThat(od -> od.getCount() == orderItem.getCount()
+                && od.getMenu().getName().equals(orderItem.getMenu().getName())),
+            argThat(u -> u.equals(userId)),
+            argThat(s -> s.equals(storeId)));
 
 
   }
 
   @Test
   @DisplayName("장바구니에 메뉴 넣기 성공 : 이미 장바구니가 채워져있는 경우")
-  private void addOrderItemSuccess2() {
+  void addOrderItemSuccess2() {
     // Arrange
     CreateOrderItemRequestDTO createOrderItemRequestDTO
         = makeCreateOrderItemRequestDTO();
@@ -127,10 +127,12 @@ public class CartServiceTest {
     Long menuId = orderItem.getMenu().getId();
 
     doReturn(storeId)
-        .when(cartDAO.findStoreIdByUserId(userId));
+        .when(cartDAO)
+        .findStoreIdByUserId(userId);
 
     doReturn(false)
-        .when(cartDAO.existOrderItem(menuId, userId));
+        .when(cartDAO)
+        .existOrderItem(menuId, userId);
 
     // Act, Assert
     cartService.addOrderItem(createOrderItemRequestDTO);
@@ -139,15 +141,19 @@ public class CartServiceTest {
         .isFalse();
 
     verify(cartDAO, times(1))
-        .addOrderItem(argThat(od -> od.getId() == orderItem.getId()
-                && od.getCount() == orderItem.getCount()
-                && od.getMenu().equals(orderItem.getMenu())),
-            argThat(u -> u == userId),
-            argThat(s -> s == storeId));
+        .addOrderItem(argThat(od -> od.getCount() == orderItem.getCount()
+                && od.getMenu().getName().equals(orderItem.getMenu().getName())),
+            argThat(u -> u.equals(userId)),
+            argThat(s -> s.equals(storeId)));
+
+
+
+
+
   }
   @Test
   @DisplayName("장바구니에 메뉴 넣기 실패 : 장바구니가 채워져있을 경우 같은 가게의 메뉴만 넣을 수 있다.")
-  private void addOrderItemFail1() {
+  void addOrderItemFail1() {
 
     // Arrange
     CreateOrderItemRequestDTO createOrderItemRequestDTO
@@ -159,13 +165,14 @@ public class CartServiceTest {
     Long otherStoreId = 10L;
 
     doReturn(otherStoreId)
-        .when(cartDAO.findStoreIdByUserId(userId));
+        .when(cartDAO)
+        .findStoreIdByUserId(userId);
 
     // Act, Assert
     assertThatThrownBy(() -> {
       cartService.addOrderItem(createOrderItemRequestDTO);
     }).isInstanceOf(RuntimeException.class)
-        .hasMessage(cartService.EXCEPTION_INVALID_STORE_ID);
+        .hasMessage(CartService.EXCEPTION_INVALID_STORE_ID);
 
     assertThat(cartService.isEmptyCart(otherStoreId))
         .isFalse();
@@ -180,7 +187,7 @@ public class CartServiceTest {
 
   @Test
   @DisplayName("장바구니에 메뉴 넣기 실패 : 장바구니에는 이미 담긴 메뉴를 다시 담을 수 없다. ")
-  private void addOrderItemFail2() {
+  void addOrderItemFail2() {
     // Arrange
     CreateOrderItemRequestDTO createOrderItemRequestDTO
         = makeCreateOrderItemRequestDTO();
@@ -191,16 +198,18 @@ public class CartServiceTest {
     Long menuId = orderItem.getMenu().getId();
 
     doReturn(storeId)
-        .when(cartDAO.findStoreIdByUserId(userId));
+        .when(cartDAO)
+        .findStoreIdByUserId(userId);
 
     doReturn(true)
-        .when(cartDAO.existOrderItem(menuId, userId));
+        .when(cartDAO)
+        .existOrderItem(menuId, userId);
 
     // Act, Assert
     assertThatThrownBy(() -> {
       cartService.addOrderItem(createOrderItemRequestDTO);
     }).isInstanceOf(RuntimeException.class)
-        .hasMessage(cartService.EXCEPTION_ALREADY_EXIST_MENU);
+        .hasMessage(CartService.EXCEPTION_DUPLICATE_MENU);
 
     assertThat(cartService.isEmptyCart(storeId))
         .isFalse();
@@ -212,7 +221,7 @@ public class CartServiceTest {
 
   @Test
   @DisplayName("장바구니 메뉴 조회 성공")
-  private void readSuccess() {
+  void readSuccess() {
     // Arrange
     Long userId = 1L;
     List<OrderItem> orderItems = new ArrayList<>();
@@ -264,7 +273,7 @@ public class CartServiceTest {
 
   @Test
   @DisplayName("장바구니 메뉴 조회 실패 : 장바구니가 비어있음.")
-  private void readFail1() {
+  void readFail1() {
     // Arrange
     Long userId = 1L;
     List<OrderItem> orderItems = new ArrayList<>();
@@ -282,7 +291,7 @@ public class CartServiceTest {
       ReadCartResponseDTO readCartResponseDTO
           = cartService.read(userId);
     }).isInstanceOf(RuntimeException.class)
-        .hasMessage(cartService.EXCEPTION_EMPTY_CART);
+        .hasMessage(CartService.EXCEPTION_EMPTY_CART);
 
     verify(storeService, times(0))
         .readOneById(storeId);
