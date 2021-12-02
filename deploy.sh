739e5a6721d4f5eae38eeac0dfb5d57fca5706d7
meme2367 <meme91322367@gmail.com>
2021-12-02 14:50:54 +0900

    CD 테스트

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPOSITORY=build/libs
+PROJECT_NAME=enjoy-delivery
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+   echo "> kill -15 $CURRENT_PID"
+   kill -15 $CURRENT_PID
+   sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
