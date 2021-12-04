ea18b4f6aa32e27f40b405fed32c8c0bb357e515
meme2367 <meme91322367@gmail.com>
2021-12-04 20:56:44 +0900

    feat : Pay Service 코드 작성

---
+++ src/main/java/com/enjoydelivery/service/PayService.java
@@ -0,0 +1,17 @@
+package com.enjoydelivery.service;
+
+import com.enjoydelivery.entity.Pay;
+import com.enjoydelivery.repository.PayRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PayService {
+
+  private final PayRepository payRepository;
+  
+  public void save(Pay pay) {
+    payRepository.save(pay);
+  }
+}
